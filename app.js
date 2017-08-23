cfce37f7c3752b5c13fcebb496f21a90a34b2cb0
Cunxiang Yu <cunxiangyu@gmail.com>
2017-08-23 20:38:22 +1000

    Set up 'pug' as view engine and 'public' folder as static assets

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+// Init app
 const app = express();
 
+// Set static assets containing client side css and js files
+app.use(express.static(__dirname + '/public'));
+
+// Set bodyParser for getting client data
+app.use(bodyParser.json()); // Use json format
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Set view engine to pug, pug files implicitly stored in 'views'
 app.set('view engine', 'pug');
 
+// Set port
+app.set('port', process.env.PORT || 8000);
+
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.render('index');
 });
 
-app.listen(3000);
+const port = app.get('port');
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
