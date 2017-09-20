e0c8e6df9414cd6a2d350a3cf0d95477169d354e
Cunxiang Yu <cunxiangyu@gmail.com>
2017-09-20 20:25:10 +1000

    Added 5 products specs

--- productData.js
+++ productData.js
@@ -7,11 +7,11 @@ new Product({
   models: [
     {
     model: 'Samsung NX3000',
-    specification: 'Samsung NX3000 specs here'
+    specification: 'Compact System Camera MP4 Video MPEG4 AVC/H264 Audio AAC HDMI Contrast Detection 1point Free selection Multi Normal 21 points Closeup 35 points Face Detection Max 10 faces Object Tracking AF Auto Daylight Cloudy Fluorescent Tungsten Flash Custom Manual Micro Adjustment each 7 steps in Amber Blue Green Magenta axes Scene Panorama Beauty Face Best Face Landscape Macro Action Freeze Rich Tone Waterfall Silhouette Sunset Night Fireworks Light Trace Continuous Shot Kids Food Party Indoor Smart Filter Miniature Vignetting Sketch Fish Eye Picture Wizard Standard Vivid Portrait Landscape Forest Retro Cool Calm Classic Custom Smart Range HDR Smart Flash Auto Auto Red-eye reduction Fill-in Fill-in Red-eye reduction Curtain Curtain Off Flip-up LCD built-in Wi-Fi with NFC Smart Camera Autoshare Remote Viewfinder Pro Full manual Control Mobile Link Group Share up to 4 people Photo Beam Play back Auto Backup Samsung Link TV Link Home Monitor Bundled with SEF8A external flash'
     },
     {
       model: 'Nikon D800',
-      specification: 'Nikon D800 specs here'
+      specification: 'Single-lens reflex digital camera Image sensor cleaning Image Dust Off reference data optional Capture software Can be selected from Standard, Neutral Vivid Monochrome Portrait Landscape selected Picture Control can be modified storage for custom Picture Controls Secure Digital UHS-I compliant SDHC and SDXC memory cards Type I CompactFlash memory cards UDMA compliant Either card can be used for primary or backup storage or for separate storage of NEF and JPEG images pictures can be copied between cards Compatible with AF NIKKOR lenses including type G and D lenses some restrictions apply to PC-NIKKOR lenses DX lenses AI-P NIKKOR lenses and non-CPU AI lenses exposure modes A and M only IX-NIKKOR lenses lenses for the F3AF and non-AI lenses cannot be used The electronic rangefinder can be used with lenses that have a maximum aperture of or faster employing eleven focus points with lenses that have a maximum aperture of f/8 or faster Nikon Advanced Multi-CAM 3500FX'
     }
   ]
 }).save();
@@ -21,7 +21,7 @@ new Product({
   models: [
     {
       model: 'Samsung S3',
-      specification: 'Samsung S3 specs here'
+      specification: 'The phone comes with a 4.80-inch touchscreen display with a resolution of 720 pixels by 1280 pixels at a PPI of 306 pixels per inch Samsung Galaxy S III price in India starts from Rs. 30,318 The Samsung Galaxy S III is powered by 1.4GHz quad-core processor and it comes with 1GB of RAM The phone packs 16GB of internal storage that can be expanded up to 64GB via a microSD card As far as the cameras are concerned the Samsung Galaxy S III packs a 8-megapixel primary camera on the rear and a 1.9-megapixel front shooter for selfies The Samsung Galaxy S III runs Android 4 and is powered by a 2100mAh removable battery It measures 136.60 x 70.60 x 8.60 height x width x thickness and weigh 133.00 grams The Samsung Galaxy S III is a single SIM GSM smartphone that accepts a Micro-SIM. Connectivity options include Wi-Fi GPS Bluetooth NFC USB OTG FM 3G and 4G Sensors on the phone include Compass Magnetometer Proximity sensor Accelerometer Ambient light sensor Gyroscope'
     }
   ]
 }).save();
@@ -31,11 +31,11 @@ new Product({
   models: [
     {
       model: 'HP Officejet Pro X576dw Multifunction',
-      specification: 'HP Officejet Pro X576dw Multifunction specs here'
+      specification: 'Document send function, save to USB flash drive scan to e-mail scan to network Google Cloud Print ready HP ePrint HP Auto Wireless Connect built-in 802.11 b/g access point Touch screen Wired Produce quality prints with Original HP inks and save up to 50% on printing costs versus colour lasers Print at record speeds—certified by Guinness World Records as the world fastest desktop printer New General Office mode delivers high-quality prints at top speeds while saving ink time, and money Print speed ISO Laser comparable Professional Mode Up to 42 ppm General Office Mode Up to 70 ppm Recommended monthly page volume 1000 to 4200 Duty cycle monthly A4 Up to 75,000 pages Print technology HP Pagewide Technology with pigmented inks Display 4.3 CGD color graphics touchscreen Connectivity standard 2 Hi-Speed USB 2.0 Host 1 Hi-Speed USB 2.0 Device 1 Ethernet 10/100 Base-TX network 1 RJ-11 modem port 802.11 b/g/n Station 802.11 b/g Access Point'
     },
     {
       model: 'Dell B1163w Mono Laser Multifunction',
-      specification: 'Dell B1163w Mono Laser Multifunction specs here'
+      specification: 'Prints scans copies Small size Wi-Fi Reasonably high-quality output across the board Meant as a personal MFP the Dell Mono Multifunction Printer B1163w is also suitable for a micro office for light-duty printing and still lighter-duty scanning and copying Easily manage your documents with NuanceTM PaperPort 14 This included software helps you transform paper documents into high-quality searchable PDF files for easy archivin, organizing and sharing PaperPort 14 places all PDF files on your desktop in clearly marked color-coded folders so information is always at your fingertips Using Google Cloud Print you can make your home and work printers available to you and anyone you choose from the applications you use every day Google Cloud Print works on your smartphone tablet ChromebookTM PC and other web-connected devices The Dell B1163w lets you and your team easily print documents photos images and reports directly from mobile smartphones and tablets'
     }
 
   ]
